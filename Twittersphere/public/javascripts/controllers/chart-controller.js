68b7d8444bdf5d7a40e71211ddf554981ed3d569
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-10-11 10:22:13 +1000

    Added comperative statistics

--- Twittersphere/public/javascripts/controllers/chart-controller.js
+++ Twittersphere/public/javascripts/controllers/chart-controller.js
@@ -1,60 +1,23 @@
+/** Controller for the live line chart */
 app.controller('ChartCtrl', ['$scope', 'socket', '$timeout', function($scope, socket, $timeout) {
-  // var RealTimeData = function(layers) {
-  //   this.layers = layers;
-  //   this.timestamp = ((new Date()).getTime() / 1000)|0;
-  // };
-
-  // RealTimeData.prototype.rand = function() {
-  //   return parseInt(Math.random() * 10) + 5;
-  // };
-
-  // RealTimeData.prototype.history = function(entries) {
-  //   if (typeof(entries) != 'number' || !entries) {
-  //       entries = 60;
-  //   }
-
-  //   var history = [];
-  //   for (var k = 0; k < this.layers; k++) {
-  //       history.push({ values: [] });
-  //   }
-
-  //   for (var i = 0; i < entries; i++) {
-  //     for (var j = 0; j < this.layers; j++) {
-  //       history[j].values.push({time: this.timestamp, y: this.rand()});
-  //     }
-  //     this.timestamp++;
-  //   }
-  //   return history;
-  // };
-
-  // RealTimeData.prototype.next = function() {
-  //   var entry = [];
-  //   for (var i = 0; i < this.layers; i++) {
-  //     entry.push({ time: this.timestamp, y: this.rand() });
-  //   }
-  //   this.timestamp++;
-  //   return entry;
-  //   }
-
-  // window.RealTimeData = RealTimeData;
-
-  // var liveData = new RealTimeData(1);
   var timestamp = ((new Date()).getTime() / 1000) | 0;
-  $scope.realtimeLine = [{values: [{time: timestamp, y: 0}]}];
-  // $scope.realtimeLine = liveData.history();
-  // console.log($scope.realtimeLine);
-  // $scope.realtimeLineFeed = liveData.next();
-  // console.log($scope.realtimeLineFeed);
+  
+  $scope.realtimeLine = [
+    {values: [{time: timestamp, y: 0}]},
+    {values: [{time: timestamp, y: 0}]}
+  ];
+
   $scope.getNextLiveLine = function(data) {
     console.log('getNextLiveLine()');
     $scope.realtimeLineFeed = data;
-    // $timeout($scope.getNextLiveLine, 1000);
   }
-  // $timeout($scope.getNextLiveLine, 1000);
 
   socket.on('analysis', function(data) {
     var timestamp = ((new Date()).getTime() / 1000) | 0;
-    $scope.result = [{time: timestamp, y: data.score}];
+    $scope.result = [
+      {time: timestamp, y: data.score},
+      {time: timestamp, y: data.comparative}
+    ];
     $scope.getNextLiveLine($scope.result);
   });
 
