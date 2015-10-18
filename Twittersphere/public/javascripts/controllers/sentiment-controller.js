27ebbef7fdf9162326c77582ff4f3aeb37d00ac7
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-10-18 12:34:29 +1000

    Improved the sentiment analysis view

--- Twittersphere/public/javascripts/controllers/sentiment-controller.js
+++ Twittersphere/public/javascripts/controllers/sentiment-controller.js
@@ -1,11 +1,8 @@
 /** Controller for the live line chart */
-app.controller('ChartCtrl', ['$scope', 'socket', function($scope, socket) {
+app.controller('SentimentCtrl', ['$scope', 'socket', function($scope, socket) {
   var timestamp = ((new Date()).getTime() / 1000) | 0;
   
-  $scope.realtimeLine = [
-    {values: [{time: timestamp, y: 0}]},
-    {values: [{time: timestamp, y: 0}]}
-  ];
+  $scope.realtimeLine = [{values: [{time: timestamp, y: 0}]}];
 
   $scope.getNextLiveLine = function(data) {
     $scope.realtimeLineFeed = data;
@@ -13,10 +10,8 @@ app.controller('ChartCtrl', ['$scope', 'socket', function($scope, socket) {
 
   socket.on('analysis', function(data) {
     var timestamp = ((new Date()).getTime() / 1000) | 0;
-    var result = [
-      {time: timestamp, y: data.score},
-      {time: timestamp, y: data.comparative}
-    ];
+    var result = [{time: timestamp, y: data.score}];
+
     $scope.result = data;
     $scope.getNextLiveLine(result);
   });
