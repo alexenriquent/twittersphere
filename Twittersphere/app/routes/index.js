be15aeb47805f6f511438e0569350f0bba09c706
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-10-18 12:32:10 +1000

    Added a comment

--- Twittersphere/app/routes/index.js
+++ Twittersphere/app/routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res) {
   res.render('index', { });
 });
 
+/** Redirect all other routes to index.html */
 router.get('/*', function(req, res) {
   res.render('index', { });
 });
