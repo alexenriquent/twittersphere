4801b6447708844472f449cf2229616a30cbc1e6
Thanat Chokwijitkul <alexenriquent@gmail.com>
2015-10-23 16:24:00 +1000

    Training the classifier with AFINN-165 and AFINN-emoticon-8

--- Twittersphere/app/models/sentiment.js
+++ Twittersphere/app/models/sentiment.js
@@ -1,7 +1,8 @@
-/** Required dependency */
+/** Required dependencies */
 var sentiment = require('sentiment');
+var training = require('./training');
 
 /** Export the module */
 module.exports = function(tweet) {
-  return sentiment(tweet);
+  return sentiment(tweet, training);
 };
